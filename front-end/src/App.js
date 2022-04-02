a17c1d8e956bd5580b00079ef73c682009c7284a
jaebin1234 <jaebin1234>
2022-04-03 04:29:02 +0900

    init:4/3, project init

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+//최상위 컴포넌트 App
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      최상위 컴포넌트 App
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
