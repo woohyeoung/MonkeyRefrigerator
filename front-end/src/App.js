f2a0c41171b34f862495df378369810af33911a5
jaebin1234 <jaebin1234>
2022-04-17 02:02:20 +0900

    rebase:4/17, rebase-1

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,31 +1,25 @@
-// import logo from './logo.svg';
-import React, { useContext, useEffect, useState } from "react";
-import "./App.css";
-//redux
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import ReduxThunk from "redux-thunk";
-import rootReducer from "./store/reducers/indexReducer";
+import React, { useContext } from 'react';
 
-//Header
-import Header from "./components/Header";
+import './App.css';
+//redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import rootReducer from './store/reducers/indexReducer';
 
 //Main
-import Main from "./components/Main";
+import Main from './components/Main';
 
 //store
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 //최상위 컴포넌트 App
 function App() {
-  return (
-    <Provider store={store}>
-      <Header />
-      <div className="container" style={{ background: "#fcf6f5ff" }}>
-        <Main />
-      </div>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<Main />
+		</Provider>
+	);
 }
 
 export default App;
--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,103 +1,105 @@
 //Header.js
-import React, { useEffect, useState } from "react";
-import "./Header.css";
+import React, { useEffect, useState } from 'react';
+import './Header.css';
 import {
-  mdiMagnify,
-  mdiFridgeOutline,
-  mdiAccount,
-  mdiFridge,
-  mdiAccountArrowRight,
-  mdiClipboardText,
-  mdiClipboardPlus,
-} from "@mdi/js";
+	mdiMagnify,
+	mdiFridgeOutline,
+	mdiAccount,
+	mdiFridge,
+	mdiAccountArrowRight,
+	mdiClipboardText,
+	mdiClipboardPlus,
+	mdiMenu,
+} from '@mdi/js';
 
 import {
-  MailOutlined,
-  AppstoreOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-import Icon from "@mdi/react";
-import { Menu } from "antd";
-import logoImg from "../assets/monkey_2.png";
-import Navigator from "./Navigator";
-import { IsLogin } from "./user/IsLogin";
+	MailOutlined,
+	AppstoreOutlined,
+	SettingOutlined,
+} from '@ant-design/icons';
+import Icon from '@mdi/react';
+import { Menu } from 'antd';
+import logoImg from '../assets/monkey_2.png';
+import Navigator from './Navigator';
+import { IsLogin } from './user/IsLogin';
 
 //router
-import { Link, useHistory } from "react-router-dom";
+import { Link, useHistory } from 'react-router-dom';
 
 function Header() {
-  const [isLogin, setIsLogin] = useState(false);
-  const history = useHistory();
-  useEffect(() => {
-    setIsLogin(IsLogin);
-  }, [IsLogin]);
-  return (
-    <>
-      <header>
-        <div className="header">
-          <div className="row">
-            <Link to="/">
-              <div className="h-logo">
-                <img src={logoImg} width={90} height={90}></img>
-              </div>
-            </Link>
-            <div className="h-title">
-              <span>Monkey Refrigerator</span>
-            </div>
-            {/* antd nav */}
-            <div className="h-menu">{/* <Navigator /> */}</div>
+	const [isLogin, setIsLogin] = useState(false);
+	const history = useHistory();
+	useEffect(() => {
+		setIsLogin(IsLogin);
+	}, [IsLogin]);
+	const onClickLogo = () => {};
+	return (
+		<>
+			<header>
+				<div className="header">
+					<div className="row">
+						<Link to="/">
+							<div className="h-logo">
+								<img src={logoImg} width={90} height={90}></img>
+							</div>
+						</Link>
+						<div className="h-title">
+							<span>Monkey Refrigerator</span>
+						</div>
+						{/* antd nav */}
+						<div className="h-menu">{/* <Navigator /> */}</div>
 
-            <div className="h-icon ">
-              {isLogin ? (
-                <>
-                  <Link to="/board">
-                    <Icon
-                      path={mdiClipboardText}
-                      title="board"
-                      size={2}
-                      color="white"
-                    />
-                  </Link>
-                  <Icon
-                    path={mdiMagnify}
-                    title="search"
-                    size={2}
-                    color="white"
-                  />
-                  <Icon
-                    path={mdiClipboardPlus}
-                    title="register"
-                    size={2}
-                    color="white"
-                  />
+						<div className="h-icon ">
+							{isLogin ? (
+								<>
+									<Link to="/board">
+										<Icon
+											path={mdiClipboardText}
+											title="board"
+											size={2}
+											color="white"
+										/>
+									</Link>
+									<Icon
+										path={mdiMagnify}
+										title="search"
+										size={2}
+										color="white"
+									/>
+									<Icon
+										path={mdiClipboardPlus}
+										title="register"
+										size={2}
+										color="white"
+									/>
 
-                  <Icon
-                    path={mdiFridge}
-                    title="refrigerator"
-                    size={2}
-                    color="white"
-                  />
-                  <Icon path={mdiAccount} title="profile" size={2} />
-                </>
-              ) : (
-                <>
-                  <Link to="/login">
-                    <Icon
-                      path={mdiAccountArrowRight}
-                      title="login"
-                      size={2}
-                      color={"white"}
-                    />
-                  </Link>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
-        <div></div>
-      </header>
-    </>
-  );
+									<Icon
+										path={mdiFridge}
+										title="refrigerator"
+										size={2}
+										color="white"
+									/>
+									<Icon path={mdiAccount} title="profile" size={2} />
+								</>
+							) : (
+								<>
+									<Link to="/login">
+										<Icon
+											path={mdiAccountArrowRight}
+											title="login"
+											size={2}
+											color={'white'}
+										/>
+									</Link>
+								</>
+							)}
+						</div>
+					</div>
+				</div>
+				<div></div>
+			</header>
+		</>
+	);
 }
 
 export default React.memo(Header);
