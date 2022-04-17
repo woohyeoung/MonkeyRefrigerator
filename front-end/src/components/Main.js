8bc01d6c857a8f2683dd6a7c2edc67a5247ce76c
jaebin1234 <jaebin1234>
2022-04-17 14:01:29 +0900

    feat : 4/17, header 컴포넌트 변경

--- front-end/src/components/Main.js
+++ front-end/src/components/Main.js
@@ -11,16 +11,13 @@ import './Main.css';
 import BoardList from './board/BoardList';
 import BoardCreate from './board/BoardCreate';
 import { Link, Route, Switch } from 'react-router-dom';
-import Icon from '@mdi/react';
-import Box from '@mui/material/Box';
-//Login
 import { Login } from './user/Login';
-//Signup
 import SignUp from './user/SignUp';
 import { useDispatch, useSelector } from 'react-redux';
 import { handleLogin } from '../store/actions/UserAction';
 import PublicRoute from './user/PublicRoute';
 import PrivateRoute from './user/PrivateRoute';
+import Header from './Header';
 
 function Main() {
 	const dispatch = useDispatch();
@@ -31,28 +28,27 @@ function Main() {
 	}, [dispatch]);
 	return (
 		<>
-			<div className="content">
-				<div className="container">
-					<Switch>
-						<PublicRoute restricted={false} exact path="/">
-							메인 페이지 - Router 들어가야함
-						</PublicRoute>
-						<PublicRoute
-							restricted={true}
-							component={Login}
-							path="/login"
-							exact
-						/>
-						<PublicRoute
-							restricted={true}
-							component={SignUp}
-							path="/signup"
-							exact
-						/>
-						<PrivateRoute component={BoardList} path="/board" exact />
-						<PrivateRoute component={BoardCreate} path="/create" exact />
-					</Switch>
-				</div>
+			<Header />
+			<div className="mainContainer">
+				<Switch>
+					<PublicRoute restricted={false} exact path="/">
+						메인 페이지 - Router 들어가야함
+					</PublicRoute>
+					<PublicRoute
+						restricted={true}
+						component={Login}
+						path="/login"
+						exact
+					/>
+					<PublicRoute
+						restricted={true}
+						component={SignUp}
+						path="/signup"
+						exact
+					/>
+					<PublicRoute component={BoardList} path="/board" exact />
+					<PrivateRoute component={BoardCreate} path="/create" exact />
+				</Switch>
 			</div>
 		</>
 	);
