bb0b45fa70f084ea5d67b659344880b06216bb5d
woohyeoung <dngud951108@gmail.com>
2022-04-24 20:20:57 +0900

    merge:4/24, 20:20, 최종최종

--- front-end/src/components/user/IsLogin.js
+++ front-end/src/components/user/IsLogin.js
@@ -1,9 +1,3 @@
-// import { useSelector } from "react-redux";
 import Cookies from "js-cookie";
 
 export const IsLogin = () => !!Cookies.get("accessToken");
-
-// export const IsLogin = () => {
-//   const tokenReducer = useSelector((state) => state.tokenReducer.authenticated);
-//   return tokenReducer;
-// };
